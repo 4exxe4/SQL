70546421e6b10d0c4c3b9737e4a23d8ebe24f0a1
user <user@WIN-CP209TC39O5>
2026-02-18 21:41:58 +0300

    18.02.25

--- DML/SQLQuery3 - SELECT Teachers.sql
+++ DML/SQLQuery3 - SELECT Teachers.sql
@@ -6,7 +6,7 @@ SELECT
 		,[Дата рождения]	=	birth_date
 		,[Возраст]			=	CAST(DATEDIFF(DAY, birth_date, GETDATE())/365.25 AS TINYINT)
 FROM	Teachers
-WHERE	CAST(DATEDIFF(DAY, birth_date, GETDATE())/365.25 AS TINYINT) BETWEEN
+WHERE	CAST(DATEDIFF(DAY, birth_date, GETDATE())/365.25 AS TINYINT) BETWEEN 38 AND 44
 ORDER BY	[Возраст] 	ASC
 --ASC	- Ascinding (По возрастанию)
 --DESC	- Descending(По убыванию)
---
+++ DML/SQLQuery4 - AggregateStudentsAndGroups.sql
@@ -0,0 +1,21 @@
+--SQLQuery4 - AggregateStudentsAndGroups.sql
+USE PV_521_Import;
+
+SELECT
+	direction_name				AS N'Направление обучения'
+	,COUNT(DISTINCT group_id)	AS N'Количество групп'
+	,COUNT(stud_id)				AS N'Количество студентов'
+FROM	Students, Groups, Directions
+WHERE	[group]		=	group_id
+AND		direction	=	direction_id
+GROUP BY direction_name
+;
+
+SELECT
+		direction_name				AS N'Направление обучения'
+		--,COUNT(stud_id)				AS N'Количество студентов'
+		--,COUNT(DISTINCT group_id)	AS N'Количество групп'	--DISTINCT выбирает только уникальные записи
+		,(SELECT COUNT(group_id) FROM Groups	WHERE direction = direction_id) AS N'Количество групп'
+		,(SELECT COUNT(stud_id) FROM Students,Groups WHERE [group] = group_id AND direction = direction_id) AS N'Количество студентов'
+FROM	Directions
+;
